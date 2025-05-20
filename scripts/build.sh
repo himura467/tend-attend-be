af30c558e219734581a62f2800a18dc3eb65eb6a
himura467 <mitarashidango0927@gmail.com>
2025-05-21 04:13:43 +0900

    refactor: update Dockerfile to use poetry for dependency management and switch base image to amazonlinux:2023

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,18 @@ set -e
 
 ROOT_DIR=$(cd $(dirname $0)/..; pwd)
 
-bash ${ROOT_DIR}/scripts/export-requirements.sh
-docker build -f ${ROOT_DIR}/docker/server/Dockerfile -t tend-attend --no-cache --provenance=false . --progress=plain
-rm ${ROOT_DIR}/requirements.txt
+PYTHON_VERSION=$(cat ${ROOT_DIR}/.python-version)
+
+rm -f app.zip
+docker build -f ${ROOT_DIR}/docker/server/Dockerfile \
+  --build-arg PYTHON_VERSION=${PYTHON_VERSION} \
+  --platform linux/amd64 \
+  --target python-libs \
+  --no-cache \
+  --provenance=false \
+  -t tend-attend:latest ${ROOT_DIR} --progress=plain
+CONTAINER_ID=$(docker create --platform linux/amd64 tend-attend:latest)
+docker cp "$CONTAINER_ID":/app ${ROOT_DIR}
+docker rm -v "$CONTAINER_ID"
+zip --recurse-paths -X app.zip ${ROOT_DIR}/app
+rm -rf ${ROOT_DIR}/app
