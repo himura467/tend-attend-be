65583d37f3b80e1ec5029eb7d080e2cefbd81264
himura467 <mitarashidango0927@gmail.com>
2025-05-22 20:09:56 +0900

    fix: remove editable install

--- scripts/export-requirements.sh
+++ scripts/export-requirements.sh
@@ -12,4 +12,5 @@ ROOT_DIR=$(cd $(dirname $0)/..; pwd)
 echo "Running export for $1"
 cd ${ROOT_DIR}/$1
 poetry export -f requirements.txt -o requirements.txt --without-hashes
+sed -i '' '/^-e file:/d' requirements.txt
 mv requirements.txt "${ROOT_DIR}/requirements.txt"
