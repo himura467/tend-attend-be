022ba543693d5dbf7566466cda95ea3fecfeaaea
himura467 <mitarashidango0927@gmail.com>
2025-05-23 00:18:26 +0900

    feat: force replace lambda function to update source code

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-if [[ $# -ne 1 ]]; then
-  echo "Usage: $0 <aws-profile>"
+if [[ $# -ne 2 ]]; then
+  echo "Usage: $0 <aws-profile> <environment>"
   exit 1
 fi
 
@@ -11,7 +11,8 @@ export AWS_PROFILE=$1
 
 ROOT_DIR=$(cd $(dirname $0)/..; pwd)
 
-cd $ROOT_DIR/terraform
+cd $ROOT_DIR/terraform/environments/$2
 
 terraform init
-terraform apply
+# Force update of Lambda function by targeting the source_code_hash
+terraform apply -replace="module.lambda.aws_lambda_function.this"
