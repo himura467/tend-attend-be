33216b0c04559d9597271b96efb31f26c323b038
himura467 <mitarashidango0927@gmail.com>
2025-05-22 18:26:19 +0900

    feat: export requirements.txt while building

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,6 +7,7 @@ ROOT_DIR=$(cd $(dirname $0)/..; pwd)
 PYTHON_VERSION=$(cat ${ROOT_DIR}/ta-api/.python-version)
 
 rm -f app.zip
+bash ${ROOT_DIR}/scripts/export-requirements.sh
 docker build -f ${ROOT_DIR}/docker/server/Dockerfile \
   --build-arg PYTHON_VERSION=${PYTHON_VERSION} \
   --platform linux/amd64 \
@@ -18,3 +19,4 @@ docker cp "$CONTAINER_ID":/app ${ROOT_DIR}
 docker rm -v "$CONTAINER_ID"
 zip --recurse-paths -X app.zip ${ROOT_DIR}/app
 rm -rf ${ROOT_DIR}/app
+rm ${ROOT_DIR}/requirements.txt
