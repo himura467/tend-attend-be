4ee674a8c83d990e6038ba8b425c92515e692ae5
himura467 <mitarashidango0927@gmail.com>
2025-05-24 04:12:14 +0900

    feat: dependencies.zip

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,7 +6,7 @@ ROOT_DIR=$(cd $(dirname $0)/..; pwd)
 
 PYTHON_VERSION=$(cat ${ROOT_DIR}/ta-api/.python-version)
 
-rm -f python.zip main.zip
+rm -f python.zip main.zip dependencies.zip
 bash ${ROOT_DIR}/scripts/export-requirements.sh ta-api
 docker build -f ${ROOT_DIR}/docker/server/Dockerfile \
   --build-arg PYTHON_VERSION=${PYTHON_VERSION} \
@@ -16,9 +16,10 @@ docker build -f ${ROOT_DIR}/docker/server/Dockerfile \
   -t tend-attend:latest ${ROOT_DIR} --progress=plain
 CONTAINER_ID=$(docker create --platform linux/amd64 tend-attend:latest)
 docker cp "$CONTAINER_ID":/python ${ROOT_DIR}
-docker cp "$CONTAINER_ID":/app ${ROOT_DIR}
+docker cp "$CONTAINER_ID":/main.py ${ROOT_DIR}
+docker cp "$CONTAINER_ID":/dependencies ${ROOT_DIR}
 docker rm -v "$CONTAINER_ID"
 zip -r -X python.zip python -x "*/__pycache__/*"
-zip -r -X main.zip app/main.py
-rm -rf ${ROOT_DIR}/python ${ROOT_DIR}/app
-rm ${ROOT_DIR}/requirements.txt
+zip -r -X main.zip main.py
+cd dependencies && zip -r -X ../dependencies.zip python -x "*/__pycache__/*" && cd ..
+rm -rf ${ROOT_DIR}/python ${ROOT_DIR}/main.py ${ROOT_DIR}/dependencies ${ROOT_DIR}/requirements.txt
