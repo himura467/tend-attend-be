a8d3e8470a0cbd8a77e1c57f82c0d835bb621b3f
himura467 <mitarashidango0927@gmail.com>
2025-05-25 02:40:28 +0900

    feat: ml server build

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,8 +4,8 @@ set -e
 
 ROOT_DIR=$(cd $(dirname $0)/..; pwd)
 
+# Server build
 PYTHON_VERSION=$(cat ${ROOT_DIR}/ta-api/.python-version)
-
 rm -f python.zip main.zip dependencies.zip
 bash ${ROOT_DIR}/scripts/export-requirements.sh ta-api
 docker build -f ${ROOT_DIR}/docker/server/Dockerfile \
@@ -23,3 +23,11 @@ zip -r -X python.zip python -x "*/__pycache__/*"
 zip -r -X main.zip main.py
 cd dependencies && zip -r -X ../dependencies.zip python -x "*/__pycache__/*" && cd ..
 rm -rf ${ROOT_DIR}/python ${ROOT_DIR}/main.py ${ROOT_DIR}/dependencies ${ROOT_DIR}/requirements.txt
+
+# ML Server build
+docker build -f ${ROOT_DIR}/docker/ml-server/Dockerfile \
+  --target production \
+  --platform linux/amd64 \
+  --no-cache \
+  --provenance=false \
+  -t tend-attend-ml:latest ${ROOT_DIR} --progress=plain
