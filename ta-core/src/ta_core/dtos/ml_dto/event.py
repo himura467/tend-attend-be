7734d98823afc9be0da098a5ac0f69778ced3bf1
himura467 <mitarashidango0927@gmail.com>
2025-05-26 03:15:33 +0900

    feat: ml dto

---
+++ ta-core/src/ta_core/dtos/ml_dto/event.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+
+from ta_core.features.event import AttendanceAction, Frequency
+
+
+class RecurrenceRule(BaseModel):
+    id: str
+    freq: Frequency
+
+
+class Recurrence(BaseModel):
+    id: str
+    rrule: RecurrenceRule
+
+
+class Event(BaseModel):
+    id: str
+    user_id: int
+    start: datetime
+    end: datetime
+    timezone: str
+    recurrence: Recurrence
+
+
+class EventAttendanceActionLog(BaseModel):
+    id: str
+    user_id: int
+    event_id: str
+    start: datetime
+    action: AttendanceAction
+    acted_at: datetime
