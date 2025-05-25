65a2834e1d2f25d3d63b50c3a0c4a1a985c1f9e8
himura467 <mitarashidango0927@gmail.com>
2025-05-26 00:10:27 +0900

    feat: cloud run for prod environment

--- terraform/environments/prod/variables.tf
+++ terraform/environments/prod/variables.tf
@@ -10,6 +10,21 @@ variable "aws_region" {
   default     = "ap-northeast-1"
 }
 
+variable "google_project_id" {
+  description = "Google Cloud project ID"
+  type        = string
+}
+
+variable "google_service_account_email" {
+  description = "Service account email for Google Cloud"
+  type        = string
+}
+
+variable "google_region" {
+  description = "Google region"
+  type        = string
+}
+
 variable "domain_name" {
   description = "Domain name"
   type        = string
