ba2ba26327dc24a98e5f8def22dfea103ff8a645
himura467 <mitarashidango0927@gmail.com>
2025-06-14 02:56:05 +0900

    feat: Add ECR lifecycle policy to retain only the last 2 images

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -3,3 +3,23 @@ resource "aws_ecr_repository" "qrcode_server" {
   force_delete         = true
   image_tag_mutability = "IMMUTABLE"
 }
+
+data "aws_ecr_lifecycle_policy_document" "qrcode_server" {
+  rule {
+    priority    = 1
+    description = "Keep only the last 2 images"
+    selection {
+      tag_status   = "any"
+      count_type   = "imageCountMoreThan"
+      count_number = 2
+    }
+    action {
+      type = "expire"
+    }
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "qrcode_server" {
+  repository = aws_ecr_repository.qrcode_server.name
+  policy     = data.aws_ecr_lifecycle_policy_document.qrcode_server.json
+}
