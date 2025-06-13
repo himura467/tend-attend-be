9360187c162d085c630e28af0d2b87d1da3ff58e
himura467 <mitarashidango0927@gmail.com>
2025-06-14 02:42:33 +0900

    chore: stop docker push on every terraform apply

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -2,25 +2,3 @@ resource "aws_ecr_repository" "qrcode_server" {
   name         = var.qrcode_server_repository
   force_delete = true
 }
-
-resource "terraform_data" "docker_push" {
-  triggers_replace = [timestamp()]
-  provisioner "local-exec" {
-    command = <<EOF
-      echo "Logging in to Amazon ECR..."
-      aws ecr get-login-password --region ${var.aws_region} | docker login --username AWS --password-stdin ${aws_ecr_repository.qrcode_server.repository_url}
-
-      echo "Tagging ${var.qrcode_server_repository} image..."
-      docker tag ${var.qrcode_server_repository}:latest ${aws_ecr_repository.qrcode_server.repository_url}:latest
-
-      echo "Pushing ${var.qrcode_server_repository} image to Amazon ECR..."
-      docker push ${aws_ecr_repository.qrcode_server.repository_url}:latest
-    EOF
-  }
-  depends_on = [aws_ecr_repository.qrcode_server]
-}
-
-resource "time_sleep" "wait_for_push" {
-  depends_on      = [terraform_data.docker_push]
-  create_duration = "30s"
-}
