edf929dde0d698de4ba2dfc40a7098873f9dfcd2
himura467 <mitarashidango0927@gmail.com>
2025-06-19 03:08:06 +0900

    refactor: remove default values from Terraform variables and add TF_VAR environment variables

--- terraform/environments/prod/variables.tf
+++ terraform/environments/prod/variables.tf
@@ -1,25 +1,21 @@
 variable "aws_region" {
   description = "AWS region"
   type        = string
-  default     = "ap-northeast-1"
-}
-
-variable "google_project_id" {
-  description = "Google Cloud project ID"
-  type        = string
-  default     = "tend-attend"
 }
 
 variable "google_service_account_email" {
   description = "Service account email for Google Cloud"
   type        = string
-  default     = "mitarashidango0927@gmail.com"
+}
+
+variable "google_project_id" {
+  description = "Google Cloud project ID"
+  type        = string
 }
 
 variable "google_region" {
   description = "Google region"
   type        = string
-  default     = "asia-northeast1"
 }
 
 variable "domain_name" {
